1e7c6784dacf7944dd08e19014146ffb944f3f04
Omar Raafat <47388390+omarraafat14@users.noreply.github.com>
2022-10-18 03:34:27 +0200

    Delete security key

--- libmanage/settings.py
+++ libmanage/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!_9k1urq_1m@#-js7rr03vb9zvp2#q%9^0hu!t+s3qs%efjn28'
+#SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
